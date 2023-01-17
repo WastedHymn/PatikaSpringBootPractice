23314b888b2026abbbe0c206cc1c01fc2ff32856
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-01-17 19:37:00 +0300

    Initial commit

---
+++ src/test/java/com/kadiryuksel/PatikaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadiryuksel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
